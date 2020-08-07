c5317cace49c04d37e74788723d22d808a6bc4c5
Corey <simon8888xsimon8888x@gmail.com>
2020-08-07 13:45:50 +0800

    feature: add new/edit.hbs template

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.engine('handlebars', exphbs({ defaultLayout: "main" }))
 app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('new')
 })
 
 app.listen(post, () => {
