eadef6d958e05fe394e77bc1ad17dda2fac6dd7b
Corey <simon8888xsimon8888x@gmail.com>
2020-08-08 21:09:16 +0800

    fix: heroku db

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ require('./config/mongoose.js')
 
 const app = express()
 const post = process.env.PORT || 3000
+if (process.env.PORT) {
+  require('./models/seeds/categorySeeder')
+  require('./models/seeds/recordSeeder')
+}
+// console.log('test-db')
 
 app.engine('handlebars', exphbs({ defaultLayout: "main" }))
 app.set('view engine', 'handlebars')
