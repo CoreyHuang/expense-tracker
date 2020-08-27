2a900fbaded1afdc901aa87cb7c1017cb576769d
Corey <simon8888xsimon8888x@gmail.com>
2020-08-27 16:44:09 +0800

    feat: add login/register hbs

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,11 @@ const router = express.Router()
 const newRoute = require('./modules/new')
 const filterRoute = require('./modules/filter')
 const homeRoute = require('./modules/home')
+const users = require('./modules/users')
 
 router.use('/new', newRoute)
 router.use('/filter', filterRoute)
+router.use('/users', users)
 router.use('/', homeRoute)
 
 module.exports = router
