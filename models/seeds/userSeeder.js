b3ead03c457390ea3243660465769fd3d38ebe75
Corey <simon8888xsimon8888x@gmail.com>
2020-08-28 16:37:37 +0800

    feat: add userSeeder

---
+++ models/seeds/userSeeder.js
@@ -0,0 +1,82 @@
+const userSchema = require('../user.js')
+const recordSchema = require('../record.js')
+const db = require('../../config/mongoose.js')
+const bcryptjs = require('bcryptjs')
+
+const userAccount = {
+  name: "這裡是哪",
+  email: "test@example.com",
+  password: "123",
+}
+
+let userData = [
+  {
+    name: '午餐',
+    category: '餐飲食品',
+    date: '2020-04-23',
+    amount: '60',
+  },
+
+  {
+    name: '晚餐',
+    category: '餐飲食品',
+    date: '2020-05-23',
+    amount: '60',
+  },
+
+  {
+    name: '捷運',
+    category: '交通出行',
+    date: '2020-04-23',
+    amount: '120',
+  },
+
+  {
+    name: '電影 : 驚奇隊長',
+    category: '休閒娛樂',
+    date: '2020-04-23',
+    amount: '220',
+  },
+
+  {
+    name: '租金',
+    category: '家居物業',
+    date: '2020-05-01',
+    amount: '25000',
+  },
+]
+
+db.once('open', async () => {
+  try {
+    const account = await createUser(userAccount)
+    await createUserDate(userData, account)
+    process.exit()
+  }
+  catch (e) { console.log(e) }
+})
+
+
+function createUser (userAccount) {
+  return new Promise((resolve, reject) => {
+    bcryptjs.genSalt(10)
+      .then(salt => bcryptjs.hash(userAccount.password, salt))
+      .then(hash => {
+        userAccount.password = hash
+        userSchema.create(userAccount)
+          .then((user) => resolve(user))
+          .catch(() => reject('Account create error'))
+      })
+      .catch(() => reject('password hash error'))
+  })
+}
+
+
+function createUserDate (userData, user) {
+  return new Promise((resolve, reject) => {
+    Promise.all(Array.from({ length: userData.length }, (x, i) =>
+      recordSchema.create(Object.assign(userData[i], { userId: user._id }))
+    ))
+      .then(() => resolve())
+      .catch(() => reject('user data create error'))
+  })
+}
