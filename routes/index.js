9e4ad802788bab8cad0daec39f1670a8852dd4b1
Corey <simon8888xsimon8888x@gmail.com>
2020-08-28 22:54:41 +0800

    refactor: remove routes/filter and put in routes/home

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,12 @@ const router = express.Router()
 
 
 const newRoute = require('./modules/new')
-const filterRoute = require('./modules/filter')
 const homeRoute = require('./modules/home')
 const users = require('./modules/users')
 const auth = require('../middleware/auth')
 const authFacebook = require('./modules/authFacebook.js')
 
 router.use('/new', auth, newRoute)
-router.use('/filter', auth, filterRoute)
 router.use('/users', users)
 router.use('/auth', authFacebook)
 router.use('/', auth, homeRoute)
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,7 +6,6 @@ const recordSchema = require('../../models/record.js')
 
 
 router.get('/', (req, res, next) => {
-  // console.log('req.user- 2', req.user)
   let totalAmount = 0
   const userId = req.user._id
 
@@ -31,7 +30,29 @@ router.get('/', (req, res, next) => {
     .catch(error => { console.log(error) })
 })
 
+router.get('/filter', (req, res, next) => {
+  const query = req.query.filter
+  const date = req.query.date
+  let totalAmount = 0
+  const userId = req.user._id
 
+  categorySchema.find().lean()
+    .then(category => {
+      recordSchema.find({ userId, category: { $regex: query }, date: { $regex: date } }).lean()
+        .then(expense => {
+          let categoryFind = {}
+          expense.forEach((record, index) => {
+            categoryFind = category.find(category => category.category === record.category)
+            expense[index].icon = categoryFind.icon
+            if ((index + 1) % 2 === 1) expense[index].judgeStyle = (index + 1) % 2
+          })
+          expense.forEach(data => totalAmount += Number(data.amount))
+          res.render('index', { recordRefactor: expense, category, totalAmount, query, date })
+        })
+        .catch(error => { console.log(error) })
+    })
+    .catch(error => { console.log(error) })
+})
 
 
 router.get('/:id', (req, res, next) => {
