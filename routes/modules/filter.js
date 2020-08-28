3cd71e65a9ca8ff9abceaf8122906c47937ad85f
Corey <simon8888xsimon8888x@gmail.com>
2020-08-28 15:15:23 +0800

    refactor: routes/filter

--- routes/modules/filter.js
+++ routes/modules/filter.js
@@ -8,30 +8,19 @@ const recordSchema = require('../../models/record.js')
 router.get('/', (req, res, next) => {
   const query = req.query.filter
   const date = req.query.date
-  console.log('query', req.query.filter)
-  console.log('date', req.query.date)
   let totalAmount = 0
   const userId = req.user._id
 
   categorySchema.find().lean()
     .then(category => {
-      recordSchema.find({ userId }).lean()
+      recordSchema.find({ userId, category: { $regex: query }, date: { $regex: date } }).lean()
         .then(expense => {
-
           let categoryFind = {}
           expense.forEach((record, index) => {
             categoryFind = category.find(category => category.category === record.category)
             expense[index].icon = categoryFind.icon
             if ((index + 1) % 2 === 1) expense[index].judgeStyle = (index + 1) % 2
           })
-          return expense
-        })
-        .then((expense) => {
-          const [year, month] = date.split('-')
-          date ? expense = expense.filter(data => data.category.includes(query)
-            && year === data.date.split('-')[0]
-            && month === data.date.split('-')[1])
-            : expense = expense.filter(data => data.category.includes(query))
           expense.forEach(data => totalAmount += Number(data.amount))
           res.render('index', { recordRefactor: expense, category, totalAmount, query, date })
         })
